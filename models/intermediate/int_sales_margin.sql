530e7a5bbaf2b2283bdf93da8fe80f41be89a504
THOMAS BOURJADE <thomas.bourjade@gmail.com>
2025-02-18 14:21:10 +0000

    Challenge 4

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,8 @@
+SELECT 
+    sales.*, 
+    product.*, 
+    (sales.quantity * product.purchase_price) AS total_purchase_price,
+    ROUND(sales.revenue - (sales.quantity * product.purchase_price), 2) AS margin
+FROM {{ ref('stg_raw__sales') }} AS sales
+LEFT JOIN {{ ref('stg_raw__product') }} AS product 
+    ON sales.products_id = product.products_id
